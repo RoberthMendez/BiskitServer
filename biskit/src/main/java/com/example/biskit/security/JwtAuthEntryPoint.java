1df38e7bc3146953d1f06cd626b59ad6ad6e3d51
Gudenxxo <sebastianguden@gmail.com>
2026-05-14 13:27:25 -0500

    jwt primero

---
+++ biskit/src/main/java/com/example/biskit/security/JwtAuthEntryPoint.java
@@ -0,0 +1,22 @@
+package com.example.biskit.security;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+
+public class JwtAuthEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized");
+    }
+
+
+    
+}
