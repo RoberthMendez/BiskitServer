f374570fe8ce28f483adcd843f5abf34f563c791
Gudenxxo <sebastianguden@gmail.com>
2026-05-14 13:45:23 -0500

    Fix Ruta especialeidades

--- biskit/src/main/java/com/example/biskit/controller/Vets/EspecialidadesController.java
+++ biskit/src/main/java/com/example/biskit/controller/Vets/EspecialidadesController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
-@RequestMapping("/vet/especialidades")
+@RequestMapping("/especialidades")
 @CrossOrigin(origins = "http://localhost:4200")
 public class EspecialidadesController {
 
