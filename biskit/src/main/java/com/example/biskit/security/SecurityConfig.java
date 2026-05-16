8a750df2271f990b095400e24ac1e978a4e8e1df
luzasalazar <luza_salazar@javeriana.edu.co>
2026-05-15 21:18:23 -0500

    Corrección filtro de seguridad

--- biskit/src/main/java/com/example/biskit/security/SecurityConfig.java
+++ biskit/src/main/java/com/example/biskit/security/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-// import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -59,7 +59,7 @@ public class SecurityConfig {
       )
       .exceptionHandling(exception -> exception.authenticationEntryPoint(jwtAuthEntryPoint));
 
-    // http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+    http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     return http.build();
   }
 
@@ -80,8 +80,8 @@ public class SecurityConfig {
     return authenticationConfiguration.getAuthenticationManager();
   }
 
-  // @Bean
-  // public JWTAuthenticationFilter jwtAuthenticationFilter() {
-  //   return new JWTAuthenticationFilter();
-  // }
+  @Bean
+  public JWTAuthenticationFilter jwtAuthenticationFilter() {
+    return new JWTAuthenticationFilter();
+  }
 }
